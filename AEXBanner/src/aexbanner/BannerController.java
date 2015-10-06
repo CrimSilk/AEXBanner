3cf7f22250f652e4b00086bbf8ab0056bd5c18ff
Jasper <jasper.rouwhorst93@gmail.com>
2015-10-06 14:37:09 +0200

    UpdateBannerTask added
    UpdateBannerTask gets the stockvalues from an IEffectenbeurs object

---
+++ AEXBanner/src/aexbanner/BannerController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aexbanner;
+
+import java.util.Timer;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Jasper Rouwhorst
+ */
+public class BannerController implements Initializable {
+    
+    private AEXBanner banner;
+    private IEffectenBeurs effectenbeurs;
+    private Timer pollingTimer;
+
+    public BannerController(AEXBanner banner) {
+
+        this.banner = banner;
+        this.effectenbeurs = new MockEffectenbeurs();
+        
+        // Start polling timer: update banner every two seconds
+        pollingTimer = new Timer(new UpdateBannerTask(effectenbeurs), 2000);
+        // TODO
+    }
+
+    // Stop banner controller
+    public void stop() {
+        pollingTimer.cancel();
+        // Stop simulation timer of effectenbeurs
+        // TODO
+    }
+ 
+    
+}
---
+++ AEXBanner/src/aexbanner/UpdateBannerTask.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aexbanner;
+
+import java.util.TimerTask;
+import java.util.List;
+
+/**
+ *
+ * @author Jasper Rouwhorst
+ */
+public class UpdateBannerTask extends TimerTask {
+
+    private IEffectenBeurs beurs;
+    private List<IFonds> fondslist;
+    
+    public UpdateBannerTask(IEffectenBeurs beurs){
+        this.beurs = beurs;
+    }
+    
+    @Override
+    public void run() {
+        fondslist = beurs.getKoersen();
+        String koersen = "";
+        for(IFonds fonds : fondslist){
+            koersen += fonds.getNaam() + " " + fonds.getKoers().toString() + " - ";
+        }
+        AEXBanner.setKoersen(koersen);
+    }
+    
+}
