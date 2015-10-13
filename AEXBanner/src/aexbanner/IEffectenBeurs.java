e623658a8675d0bf8ee702c340ee7a4654b665ea
Jasper <jasper.rouwhorst93@gmail.com>
2015-10-13 14:13:08 +0200

    IEffectenbeurs extends Remote

--- AEXBanner/src/aexbanner/IEffectenBeurs.java
+++ AEXBanner/src/aexbanner/IEffectenBeurs.java
@@ -1,10 +1,11 @@
 package aexbanner;
 
 import java.util.List;
+import java.rmi.*;
 
 /**
  * Created by ville on 6/10/2015.
  */
-public interface IEffectenbeurs {
+public interface IEffectenbeurs extends Remote{
     List<IFonds> getKoersen();
 }
